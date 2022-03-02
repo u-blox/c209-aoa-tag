a9b520cc5363b1e6d5984130f85728536e485fd3
Sam Detweiler <sdetweil@gmail.com>
2022-03-02 10:15:58 -0700

    fix advertising rates, so not the same, per spec

--- src/bt_adv.c
+++ src/bt_adv.c
@@ -22,6 +22,7 @@
 
 LOG_MODULE_REGISTER(bt_adv, CONFIG_APPLICATION_MODULE_LOG_LEVEL);
 
+extern bool isAdvRunning;
 static uint16_t minAdvInterval;
 static uint16_t maxAdvInterval;
 
@@ -45,6 +46,9 @@ static struct bt_data ad[] = {
 };
 
 void btAdvInit(uint16_t min_int, uint16_t max_int, uint8_t* namespace, uint8_t* instance_id, int8_t txPower) {
+    if( min_int == max_int){
+      max_int++;
+    }
     minAdvInterval = min_int / 0.625;
     maxAdvInterval = max_int / 0.625;
 
@@ -67,12 +71,17 @@ void btAdvStop(void) {
     LOG_INF("Adv stopped");
 }
 
-void btAdvUpdateAdvInterval(uint16_t min, uint16_t max) {
-    minAdvInterval = min / 0.625;
-    maxAdvInterval = max / 0.625;
-    int err = bt_le_adv_stop();
-    if (err) {
-        LOG_ERR("Failed stopping adv when changing adv interval: %d", err);
+void btAdvUpdateAdvInterval(uint16_t min_int, uint16_t max_int) {
+    if( min_int == max_int){
+      max_int++;
     }
-    btAdvStart();
+    minAdvInterval = min_int / 0.625;
+    maxAdvInterval = max_int / 0.625;
+    if(isAdvRunning){
+      int err = bt_le_adv_stop();
+      if (err) {
+          LOG_ERR("Failed stopping adv when changing adv interval: %d", err);
+      }
+      btAdvStart();
+    }
 }

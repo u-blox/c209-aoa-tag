671c3a534b981a50ec837b43cca912a420d445a8
Jakob Krantz <jakob.krantz@u-blox.com>
2022-08-10 16:41:55 +0200

    Split up tag connections into chunks, as it's not possible to connect to all at once.

--- scripts/ble_tag_control.py
+++ scripts/ble_tag_control.py
@@ -9,6 +9,7 @@ import time
 from bleak import BleakScanner, BleakClient
 from bleak.backends.scanner import AdvertisementData
 from bleak.backends.device import BLEDevice
+import numpy as np
 
 # Nordic UART Service UUIDs
 UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
@@ -58,20 +59,24 @@ async def send_at_commands(device, command_list):
                 for at in command_list:
                     cmd_sent_evt.clear()
                     print("Send:", at)
-                    await client.write_gatt_char(UART_RX_CHAR_UUID, str.encode(at))
-                    try:
-                        await asyncio.wait_for(cmd_sent_evt.wait(), timeout=2)
-                    except asyncio.TimeoutError:
-                        print("Timeout error no rsp")
+                    tries = 0
+                    while (tries < 3):
+                        await client.write_gatt_char(UART_RX_CHAR_UUID, str.encode(at))
+                        try:
+                            await asyncio.wait_for(cmd_sent_evt.wait(), timeout=2)
+                            break
+                        except asyncio.TimeoutError:
+                            tries = tries+1
+                            print("Timeout error no rsp. Try:", tries)
 
                 print("Done diconnect")
                 await client.disconnect()
                 return rsps
         except Exception as e:
             num_tries = num_tries + 1
-            await asyncio.sleep(3)
             if num_tries == max_tries:
-                raise e
+                return []
+                pass
     return None
 
 
@@ -80,16 +85,17 @@ def main(devices, command_list):
 
 
 def ble_send_at_commands(devices, command_list):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
     if isinstance(devices, list):
-        print("Connect to: ", devices)
-        group = asyncio.gather(
-            *(send_at_commands(device, command_list) for device in devices)
-        )
-        results = loop.run_until_complete(group)
-        print(results)
-        loop.close()
+        slices = len(devices) / 4
+        for devices_chunk in np.array_split(devices, slices):
+            print("devices_chunk", devices_chunk)
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            group = asyncio.gather(
+                *(send_at_commands(device, command_list) for device in devices_chunk)
+            )
+            results = loop.run_until_complete(group)
+            loop.close()
         return results
     else:
         return asyncio.run(send_at_commands(devices, command_list))
